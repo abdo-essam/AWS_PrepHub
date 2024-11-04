f0c57e68241196c9d7a4875218d6318643e537a6
abdo <abdo.e.mosa@gmail.com>
2024-11-04 04:08:14 +0200

    feat(ui): add review question screen body
    Adds the body of the review question screen, including a question card and a correct answer. This screen displays a question with a close button to mark it incorrect and a correct answer to the question.

--- lib/features/review_question/screen/widget/review_question_screen_body.dart
+++ lib/features/review_question/screen/widget/review_question_screen_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ReviewQuestionScreenBody extends StatelessWidget {
   const ReviewQuestionScreenBody({super.key});
@@ -8,47 +9,78 @@ class ReviewQuestionScreenBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          iconTheme: const IconThemeData(color: Colors.white),
-          centerTitle: true,
           backgroundColor: Colors.black,
-          title: const Text(
-            "Questions With Answers",
-            style: TextStyle(color: Colors.white),
+          appBar: AppBar(
+            iconTheme: const IconThemeData(color: Colors.white),
+            centerTitle: true,
+            backgroundColor: Colors.black,
+            title: const Text(
+              "Questions With Answers",
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 15),
-            child:  const Column(
-              children: [
-                Text("Question 1" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-                Text("Question 2" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-                Text("Question 3" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-                Text("Question 4" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-                Text("Question 5" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-                Text("Question 6" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-                Text("Question 7" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-                Text("Question 8" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-                Text("Question 9" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-                Text("Question 10" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-                Text("Question 11" , style: TextStyle(color: Colors.white),),
-                SizedBox(height: 20,),
-            ],),
-          ),
-        )
-      ),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              children: [
+                                const Expanded(
+                                  child: Icon(
+                                    Icons.close,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 20.w,
+                                ),
+                                const Expanded(
+                                  flex: 20,
+                                  child: Text(
+                                    "Question 1 : What is the capital of India ? Question 1 : What is the capital of India ? Question 1 : What is the capital of India ?",
+                                    style: TextStyle(
+                                        fontSize: 18, color: Colors.black),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text("Correct: ",
+                                      style: TextStyle(
+                                          color: Colors.green,
+                                          fontWeight: FontWeight.bold)),
+                                  Text("New Delhi",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                ]),
+                          ],
+                        )),
+                  )
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
